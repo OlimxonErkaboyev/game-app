66fba58806d67c8798df72da8887acd25be41bed
Olimxon <olimxonerkaboyev0@gmail.com>
2024-06-24 17:31:58 +0500

    building game card

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,37 +1,21 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponce {
-  count: number;
-  results: Game[];
-}
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponce>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
-        setError(err.message);
-        console.log(error);
-      });
-  });
-  console.log(games);
+  const { games, error } = useGames();
   return (
     <>
-      <ul>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
--- src/services/api-client.tsx
+++ src/services/api-client.tsx
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "1d3700477408473ab40c3669b57676b2",
   },
 });
